01f300d0510ffaef51e2598ca683efac7ad688ee
Ragitha Sanjayamal <UnicornRSA@unicornsolution.com>
2018-09-12 17:26:51 +0530

    develop navbar

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyALhxKhakboK_GqZgZ0qZKGdRU3PBik9-A",
+    authDomain: "oshop-e055c.firebaseapp.com",
+    databaseURL: "https://oshop-e055c.firebaseio.com",
+    projectId: "oshop-e055c",
+    storageBucket: "oshop-e055c.appspot.com",
+    messagingSenderId: "624972618618"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyALhxKhakboK_GqZgZ0qZKGdRU3PBik9-A",
+    authDomain: "oshop-e055c.firebaseapp.com",
+    databaseURL: "https://oshop-e055c.firebaseio.com",
+    projectId: "oshop-e055c",
+    storageBucket: "oshop-e055c.appspot.com",
+    messagingSenderId: "624972618618"
+  }
 };
 
 /*
